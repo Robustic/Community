391615cc7ed5fe50d489a54f7a8a84565b1397ac
Juha Malinen <juha.malinen@helsinki.fi>
2019-12-05 21:07:53 +0200

    Follower can be blocked

--- src/main/java/projekti/controllers/OwnProfileController.java
+++ src/main/java/projekti/controllers/OwnProfileController.java
@@ -1,19 +1,15 @@
 package projekti.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import projekti.entities.Message;
-import projekti.entities.Account;
+import projekti.entities.Following;
 import projekti.entities.Profile;
-import projekti.repositories.AccountRepository;
 import projekti.repositories.FollowingRepository;
 import projekti.repositories.MessageRepository;
 import projekti.repositories.ProfileRepository;
@@ -21,19 +17,19 @@ import projekti.services.ProfileService;
 
 @Controller
 public class OwnProfileController {
-    
+
     @Autowired
     private ProfileService profileService;
-    
+
     @Autowired
     private ProfileRepository profileRepository;
-    
+
     @Autowired
     private MessageRepository messageRepository;
-    
+
     @Autowired
     private FollowingRepository followingRepository;
-        
+
     @GetMapping("/ownprofile")
     public String getOwnProfile(Model model) {
         Profile profileUsedNow = profileService.findProfileForCurrentUser();
@@ -43,7 +39,8 @@ public class OwnProfileController {
         model.addAttribute("whoAreFollowingMe", followingRepository.findByFollowedAlias(profileUsedNow.getAlias()));
         return "ownprofile";
     }
-    
+
+    ///ownprofile/{stopfollower}/stopfollower
     @PostMapping("/ownprofile")
     public String add(@RequestParam String text) {
         if (text != null && !text.trim().isEmpty()) {
@@ -52,10 +49,18 @@ public class OwnProfileController {
             Profile profileWhichUsedNow = profileService.findProfileForCurrentUser();
             message.setProfile(profileWhichUsedNow);
             message.setAlias(profileWhichUsedNow.getAlias());
-            
+
             messageRepository.save(message);
         }
 
+        return "redirect:/ownprofile";
+    }
+
+    @PostMapping("/ownprofile/{stopfollowerprofile}/stopfollower")
+    public String removeFollower(@PathVariable String stopfollowerprofile) {
+        Following followingToDelete = followingRepository.findByFollowerAliasAndFollowedAlias(
+                stopfollowerprofile, profileService.findAliasForCurrentUser());
+        followingRepository.delete(followingToDelete);
         return "redirect:/ownprofile";
     }
 }

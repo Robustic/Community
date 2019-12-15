cdbc6d7c31e5f2ed236be353f05d167a2f026aab
Juha Malinen <juha.malinen@helsinki.fi>
2019-12-15 15:14:41 +0200

    InitializeDatabase Autowired removed

--- src/main/java/projekti/controllers/DefaultController.java
+++ src/main/java/projekti/controllers/DefaultController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import projekti.InitializeDatabase;
+//import projekti.InitializeDatabase;
 import projekti.services.AccountService;
 import projekti.services.ProfileService;
 
 @Controller
 public class DefaultController {
-    @Autowired
-    private InitializeDatabase initializeDatabase;
+//    @Autowired
+//    private InitializeDatabase initializeDatabase;
     
     @Autowired
     private AccountService accountService;
